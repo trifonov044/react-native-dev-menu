6b0ebe59ce03929dd3472f4b9652aa23b4938ad5
trifonov044 <trifonov044@users.noreply.github.com>
2018-11-16 10:59:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ type RNDevMenuModule = {
 let DevMenu: RNDevMenuModule = {
   addItem(name, handler) {
     // $FlowFixMe
-    if (!__DEV__) {
+    if (global.__DEBUG__) {
+      if (!this._eventHandlers) {
+        this._eventHandlers = new Map();
+      }
+
+      this._eventHandlers.set(name, handler);
+
+      if (!this._eventEmitter) {
+        this._eventEmitter = new NativeEventEmitter(RNDevMenu);
+
+        this._eventEmitter.addListener('customDevOptionTap', (name: string) => {
+          if (this._eventHandlers) {
+            const handler = this._eventHandlers.get(name);
+
+            if (handler) {
+              handler();
+            }
+          }
+        });
+      }
+
+      return RNDevMenu.addItem(name);
+    } else {
       return Promise.resolve();
     }
-
-    if (!this._eventHandlers) {
-      this._eventHandlers = new Map();
-    }
-
-    this._eventHandlers.set(name, handler);
-
-    if (!this._eventEmitter) {
-      this._eventEmitter = new NativeEventEmitter(RNDevMenu);
-
-      this._eventEmitter.addListener('customDevOptionTap', (name: string) => {
-        if (this._eventHandlers) {
-          const handler = this._eventHandlers.get(name);
-
-          if (handler) {
-            handler();
-          }
-        }
-      });
-    }
-
-    return RNDevMenu.addItem(name);
   }
 };
 
